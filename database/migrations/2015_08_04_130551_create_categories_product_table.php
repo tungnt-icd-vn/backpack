36fbe97b5910b7d7d12e05d32c776bc5868a3ab7
tunngnt <tungnt@icd-vn.com>
2021-04-15 16:11:26 +0700

    tungnt

---
+++ database/migrations/2015_08_04_130551_create_categories_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCategoriesProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('trees_code');
+            $table->string('categories_products_code');
+            $table->string('tittle');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->enum('status', ['plan', 'planting', 'harvested', 'cancel' ])->default('plan');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories_products');
+    }
+}
